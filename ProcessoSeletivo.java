56c5bdd291d97d9b582d13b195611504059934ba
Malú <130713333+malummartiins@users.noreply.github.com>
2023-08-18 11:38:37 -0300

    update

---
+++ ProcessoSeletivo.java
@@ -0,0 +1,93 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        /*
+         * analisarCandidato(1900.0);
+         * analisarCandidato(2200.0);
+         * analisarCandidato(2000.0);
+         */
+
+        // imprimirSelecionados();
+        // selecaoCandidatos();
+
+        String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO" };
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA "+ tentativasRealizadas + " TENTATIVA");
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + " NÚMERO MAXIMO TENTAVICAS " + tentativasRealizadas + " REALIZADA");       
+    }
+
+    // método auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
+    }
+ 
+    static void imprimirSelecionados() {
+        String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO" };
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato de nº " + (indice + 1) + " é o " + candidatos[indice]);
+        }
+
+    }
+
+    static void selecaoCandidatos() {
+        String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA",
+                "DANIELA", "JORGE" };
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA CANDIDATO");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR COM CONTRA PROPOSTA");
+        } else {
+            System.out.println("AGUARDANDO RESULTADOS DOS DEMAIS CANDIDATOS");
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+## Mais um projeto feito no Bootcamp Santander
+
+Pequeno sistema para validação de processos seletivos;
+
